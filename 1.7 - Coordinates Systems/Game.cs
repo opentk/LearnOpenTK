a7d91e7ba36432ba5873c20ab04ad419f51e9f79
void <theprismaticvoid@gmail.com>
2018-11-03 13:51:34 -0500

    Start on 1.7

---
+++ 1.7 - Coordinates Systems/Game.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Drawing;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+
+namespace LearnOpenGL_TK
+{
+    class Game : GameWindow
+    {
+        int ElementBufferObject;
+        int VertexBufferObject;
+        int VertexArrayObject;
+
+        Shader shader;
+        Texture texture;
+
+
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            GL.Enable(EnableCap.DepthTest);
+
+            float[] vertices =
+            {
+                //Position          Texture coordinates
+                 0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+                 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+                -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+                -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left 
+            };
+
+            uint[] indices =
+            {
+                0, 1, 3,
+                1, 2, 3
+            };
+
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            //shader.vert has been modified. Take a look at it after the explanation in OnRenderFrame.
+            shader = new Shader("shader.vert", "shader.frag");
+            shader.Use();
+
+            texture = new Texture("container.png");
+            texture.Use(TextureUnit.Texture0);
+
+            shader.SetInt("texture0", 0);
+
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexArrayObject);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+
+
+            int vertexLocation = shader.GetAttribLocation("aPosition");
+            GL.EnableVertexAttribArray(vertexLocation);
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 5 * sizeof(float), 0);
+
+
+            int texCoordLocation = shader.GetAttribLocation("aTexCoord");
+            GL.EnableVertexAttribArray(texCoordLocation);
+            GL.VertexAttribPointer(texCoordLocation, 2, VertexAttribPointerType.Float, false, 5 * sizeof(float), 3 * sizeof(float));
+
+            base.OnLoad(e);
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.BindVertexArray(VertexArrayObject);
+
+            texture.Use(TextureUnit.Texture0);
+            shader.Use();
+
+            Matrix4 model = Matrix4.Identity * Matrix4.CreateRotationX((float)MathHelper.DegreesToRadians(55.0));
+            Matrix4 view = Matrix4.CreateTranslation(0.0f, 0.0f, -3.0f);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)MathHelper.DegreesToRadians(45.0), base.Width / base.Height, 0.1f, 100.0f);
+            shader.SetMatrix4("model", model);
+            shader.SetMatrix4("view", view);
+            shader.SetMatrix4("projection", projection);
+
+            GL.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, 0);
+
+            Context.SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            KeyboardState input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+
+
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            base.OnResize(e);
+        }
+
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
+
+            shader.Dispose();
+            texture.Dispose();
+            base.OnUnload(e);
+        }
+    }
+}
