773e386a7fe133ee68f46503ba5926a1434f95e4
void <theprismaticvoid@gmail.com>
2018-11-11 14:25:06 -0600

    Update the comments to match the web version better

--- 1.1 - Creating a Window/Game.cs
+++ 1.1 - Creating a Window/Game.cs
@@ -4,19 +4,23 @@ using OpenTK.Input;
 
 namespace LearnOpenGL_TK
 {
-    //This is where all OpenGL code will be written. For now, though, leave it blank outside of a simple constructor.
+    //This is where all OpenGL code will be written.
+    //OpenTK allows for several functions to be overriden to extend functionality; this is how we'll be writing code.
     class Game : GameWindow
     {
         //A simple constructor to let us set the width/height/title of the window.
         public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
 
-        //OpenTK allows for several functions to be overriden to extend functionality; this is how we'll be writing code.
+        //This function runs on every update frame.
         protected override void OnUpdateFrame(FrameEventArgs e)
         {
+            //Get the current state of the keyboard on this frame.
             KeyboardState input = Keyboard.GetState();
 
+            //Check if the Escape button is currently being pressed.
             if (input.IsKeyDown(Key.Escape))
             {
+                //If it is, exit the window.
                 Exit();
             }
 
