83ff9d5cbb125d168fb0c90c5f682297d0d7e959
Frederik <fjaplv64@gmail.com>
2019-04-21 11:02:42 +0200

    Initial project setup

---
+++ Colors/Window.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Input;
+using LearnOpenTK.Common;
+
+namespace LearnOpenTK
+{
+    // We now have a rotating rectangle but how can we make the view move based on the users input
+    // In this tutorial we will take a look at how you could implement a camera class
+    // and start responding to user input
+    // You can move to the camera class to see a lot of the new code added
+    // Otherwise you can move to Load to see how the camera is initialized
+    
+    // In reality we can't move the camera but we actually move the rectangle
+    // I will explain this more in depth in the web version, however it pretty much gives us the same result
+    // as if i could move the view
+    public class Window : GameWindow
+    {
+        private readonly float[] vertices =
+        {
+            // Position         Texture coordinates
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f, // top right
+             0.5f, -0.5f, 0.0f, 1.0f, 0.0f, // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, // bottom left
+            -0.5f,  0.5f, 0.0f, 0.0f, 1.0f  // top left 
+        };
+
+        private readonly uint[] indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
+
+        private int _elementBufferObject;
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+
+        private Shader shader;
+        
+        private Camera camera;
+        private bool firstMove = true;
+        private Vector2 lastPos;
+
+        public Window(int width, int height, string title) : base(width, height, GraphicsMode.Default, title) { }
+
+        
+        protected override void OnLoad(EventArgs e)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            GL.Enable(EnableCap.DepthTest);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            _elementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            shader.Use();
+
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexArrayObject);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+
+
+            var vertexLocation = shader.GetAttribLocation("aPosition");
+            GL.EnableVertexAttribArray(vertexLocation);
+            GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            camera = new Camera(Vector3.UnitZ * 3);
+            camera.AspectRatio = Width / (float)Height;
+            
+            CursorVisible = false;
+            
+            base.OnLoad(e);
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.BindVertexArray(_vertexArrayObject);
+
+            shader.Use();
+            
+            shader.SetMatrix4("model", Matrix4.Identity);
+            shader.SetMatrix4("view", camera.GetViewMatrix());
+            shader.SetMatrix4("projection", camera.GetProjectionMatrix());
+
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+
+            SwapBuffers();
+
+            base.OnRenderFrame(e);
+        }
+
+        
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            if (!Focused)
+            {
+                return;
+            }
+
+            var input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+            
+            if (input.IsKeyDown(Key.W))
+                camera.Position += camera.Front * camera.Speed * (float)e.Time; // Forward 
+            if (input.IsKeyDown(Key.S))
+                camera.Position -= camera.Front * camera.Speed * (float)e.Time; // Backwards
+            if (input.IsKeyDown(Key.A))
+                camera.Position -= camera.Right * camera.Speed * (float)e.Time; // Left
+            if (input.IsKeyDown(Key.D))
+                camera.Position += camera.Right * camera.Speed * (float)e.Time; // Right
+            if (input.IsKeyDown(Key.Space))
+                camera.Position += camera.Up * camera.Speed * (float)e.Time; // Up 
+            if (input.IsKeyDown(Key.LShift))
+                camera.Position -= camera.Up * camera.Speed * (float)e.Time; // Down
+
+            var mouse = Mouse.GetState();
+
+            if (firstMove)
+            {
+                lastPos = new Vector2(mouse.X, mouse.Y);
+                firstMove = false;
+            }
+            else
+            {
+                var deltaX = mouse.X - lastPos.X;
+                var deltaY = mouse.Y - lastPos.Y;
+                lastPos = new Vector2(mouse.X, mouse.Y);
+                
+                camera.Yaw += deltaX * camera.Sensitivity;
+                camera.Pitch -= deltaY * camera.Sensitivity;
+            }
+            
+            base.OnUpdateFrame(e);
+        }
+
+        protected override void OnMouseMove(MouseMoveEventArgs e)
+        {
+            if (Focused)
+            {
+                Mouse.SetPosition(X + Width/2f, Y + Height/2f);
+            }
+            
+            base.OnMouseMove(e);
+        }
+        
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            camera.Fov -= e.DeltaPrecise;
+            base.OnMouseWheel(e);
+        }
+
+        
+        protected override void OnResize(EventArgs e)
+        {
+            GL.Viewport(0, 0, Width, Height);
+            camera.AspectRatio = Width / (float)Height;
+            base.OnResize(e);
+        }
+
+
+        protected override void OnUnload(EventArgs e)
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteVertexArray(_vertexArrayObject);
+
+            shader.Dispose();
+
+            base.OnUnload(e);
+        }
+    }
+}
