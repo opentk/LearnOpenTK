7fc2473b7dc1f0f2ed32092fc03a3f9080a4bb43
Frederik <fjaplv64@gmail.com>
2019-04-25 20:17:15 +0200

    Support push
    Need some help so i have to share the code with the disc.

--- Chapter 2/1 - Colors/Window.cs
+++ Chapter 2/1 - Colors/Window.cs
@@ -7,36 +7,73 @@ using LearnOpenTK.Common;
 
 namespace LearnOpenTK
 {
+    //In this chapter we will focus on how to use lighting to make our games and other applications more lifelike
+
+    //In this first part the focus will mainly be on setting up a scene for testing the different coloring options.
+    //We draw two cubes one at 0,0,0 for testing our light shader, the second one is drawn where we have the light.
+    //Furthermore in the shaders we have set up some basic physically based coloring.
     public class Window : GameWindow
     {
-        private readonly float[] _vertices =
-        {
-            // Position
-             0.5f,  0.5f,  0.5f, //left-top-backward
-             0.5f,  0.5f, -0.5f, //left-top-forward
-             0.5f, -0.5f,  0.5f, //left-bottom-backward
-             0.5f, -0.5f, -0.5f, //left-bottom-forward
-            -0.5f,  0.5f,  0.5f, //right-top-backward
-            -0.5f,  0.5f, -0.5f, //right-top-forward
-            -0.5f, -0.5f,  0.5f, //right-bottom-backward
-            -0.5f, -0.5f, -0.5f  //right-bottom-forward
+        //The vertices are now used to draw cubes, notice how we dont use the texture coords anymore,
+        //although you could easily use the textures together with the lighting tutorials it isn't
+        //the main focus so we wont have them here, it could however be a fun exercise to
+        //add the textures back and see how that works later down the line.
+        private readonly float[] _vertices = {
+             //Position           Normal
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, //Front face
+             0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+             0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+            -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+            -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f, 
+
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f, //Back face
+             0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f, //Left face
+            -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+            -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f, //Right face
+             0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+             0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+             0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+             0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f, //Bottom face
+             0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+             0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+            -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+            -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f, //Top face
+             0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+             0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+            -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+            -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
         };
-        private readonly uint[] _indices =
-        {
-            0, 1, 2, 1, 2, 3, //left face
-            4, 5, 6, 5, 6, 7, //right face
-            0, 1, 4, 1, 4, 5, //top face
-            2, 3, 6, 3, 6, 7, //bottom face
-            0, 2, 4, 2, 4, 6, //backward face
-            1, 3, 5, 3, 5, 7  //forward face
-        };
+        //This is the position of both the light and the place the lamp cube will be drawn in the scene
         private readonly Vector3 _lightPos = new Vector3(1.2f, 1.0f, 2.0f);
 
-        private int _elementBufferObject;
         private int _vertexBufferObject;
+        //I renamed the vertex array object since we now want to VAO's one for the model (the big cube for testing light shaders),
+        //and one for the lamp so we can see where the light source comes from.
+        //In an actual application you would probably either dont draw the lamp at all or draw it with a model of a lamp of some sort.
         private int _vaoModel;
         private int _vaoLamp;
 
+        //We also need two shaders, one for the lamp and one for our lighting material.
+        //The lighting shader is where most of this chapter will take place as this is where a lot of the lighting "magic" happens.
         private Shader _lampShader;
         private Shader _lightingShader;
         
@@ -57,27 +94,29 @@ namespace LearnOpenTK
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
-            _elementBufferObject = GL.GenBuffer();
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
-
+            //Load the two different shaders, they use the same vertex shader program. However they have two different fragment shaders.
+            //This is because the lamp only uses a basic shader to turn it white, it wouldn't make sense to have the lamp lit in other colors.
+            //The lighting shaders uses the lighting.frag shader which is what a large part of this chapter will be about
             _lightingShader = new Shader("Shaders/shader.vert", "Shaders/lighting.frag");
             _lampShader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
 
+            //Initialize the vao for the model
             _vaoModel = GL.GenVertexArray();
             GL.BindVertexArray(_vaoModel);
 
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
 
             var vertexLocation = _lampShader.GetAttribLocation("aPos");
             GL.EnableVertexAttribArray(vertexLocation);
             GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
 
+
+            //Initialize the vao for the lamp, this is mostly the same as the code for the model cube
             _vaoLamp = GL.GenVertexArray();
             GL.BindVertexArray(_vaoLamp);
+            // we only need to bind to the VBO, the container's VBO's data already contains the correct data.
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+            // set the vertex attributes (only position data for our lamp)
             GL.EnableVertexAttribArray(vertexLocation);
             GL.VertexAttribPointer(vertexLocation, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
 
@@ -94,32 +133,34 @@ namespace LearnOpenTK
         {
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
+            //Draw the model/cube with the lighting shader
             GL.BindVertexArray(_vaoModel);
 
             _lightingShader.Use();
             
-            _lightingShader.SetMatrix4("model", Matrix4.Identity);
+            _lightingShader.SetMatrix4("model", Matrix4.Identity); //Matrix4.Identity is used as the matrix, since we just wanna draw it at 0,0,0
             _lightingShader.SetMatrix4("view", _camera.GetViewMatrix());
             _lightingShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
             
             _lightingShader.SetVector3("objectColor", new Vector3(1.0f, 0.5f, 0.31f));
             _lightingShader.SetVector3("lightColor", new Vector3(1.0f, 1.0f, 1.0f));
 
-            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
 
+            //Draw the lamp, this is mostly the same as for the model cube
             GL.BindVertexArray(_vaoModel);
             
             _lampShader.Use();
 
             Matrix4 lampMatrix = Matrix4.Identity;
-            lampMatrix *= Matrix4.CreateScale(0.2f);
+            lampMatrix *= Matrix4.CreateScale(0.2f); //We scale the lamp cube down a bit to make it less dominant
             lampMatrix *= Matrix4.CreateTranslation(_lightPos);
             
             _lampShader.SetMatrix4("model", lampMatrix);
             _lampShader.SetMatrix4("view", _camera.GetViewMatrix());
             _lampShader.SetMatrix4("projection", _camera.GetProjectionMatrix());
             
-            GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 36);
 
             SwapBuffers();
 
@@ -206,7 +247,6 @@ namespace LearnOpenTK
             GL.UseProgram(0);
 
             GL.DeleteBuffer(_vertexBufferObject);
-            GL.DeleteBuffer(_elementBufferObject);
             GL.DeleteVertexArray(_vaoModel);
             GL.DeleteVertexArray(_vaoLamp);
 
--- Chapter 2/2 - Basic lighting/Shaders/lighting.frag
+++ Chapter 2/2 - Basic lighting/Shaders/lighting.frag
@@ -3,8 +3,26 @@ out vec4 FragColor;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
+uniform vec3 lightPos;
+
+in vec3 Normal;
+in vec3 FragPos;
 
 void main()
 {
-    FragColor = vec4(lightColor * objectColor, 1.0);
+    float ambientStrength = 0.1;
+    vec3 ambient = ambientStrength * lightColor;
+
+
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPos - FragPos);
+
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+
+    vec3 result = (ambient + diffuse) * objectColor;
+    FragColor = vec4(result, 1.0);
+    
+    FragColor = vec4(Normal.rgb, 1.0);
 }
