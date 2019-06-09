13295a94ba14a3cb7f789ed6b471e63a98749e25
void <theprismaticvoid@gmail.com>
2019-06-08 19:04:50 -0500

    Replace ImageSharp with System.Drawing

--- Common/Texture.cs
+++ Common/Texture.cs
@@ -1,9 +1,7 @@
-﻿using OpenTK.Graphics.OpenGL4;
-
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Advanced;
-using SixLabors.ImageSharp.PixelFormats;
-using SixLabors.ImageSharp.Processing;
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using OpenTK.Graphics.OpenGL4;
+using PixelFormat = OpenTK.Graphics.OpenGL4.PixelFormat;
 
 namespace LearnOpenTK.Common
 {
@@ -21,25 +19,43 @@ namespace LearnOpenTK.Common
             // Bind the handle
             Use();
 
+            // For this example, we're going to use .NET's built-in System.Drawing library to load textures.
+
             // Load the image
-            using (Image<Rgba32> image = Image.Load(path))
-            {
-                // ImageSharp loads from the top-left pixel, whereas OpenGL loads from the bottom-left, causing the
-                // texture to be flipped vertically. This will correct that, making the texture display properly.
-                image.Mutate(x => x.Flip(FlipMode.Vertical));
+            using (var image = new Bitmap(path)) {
+                // First, we get our pixels from the bitmap we loaded.
+                // Arguments:
+                //   The pixel area we want. Typically, you want to leave it as (0,0) to (width,height), but you can
+                //   use other rectangles to get segments of textures, useful for things such as spritesheets.
+                //   The locking mode. Basically, how you want to use the pixels. Since we're passing them to OpenGL,
+                //   we only need ReadOnly.
+                //   Next is the pixel format we want our pixels to be in. In this case, ARGB will suffice.
+                //   We have to fully qualify the name because OpenTK also has an enum named PixelFormat.
+                var data = image.LockBits(
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    ImageLockMode.ReadOnly,
+                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
 
                 // Now that our pixels are prepared, it's time to generate a texture. We do this with GL.TexImage2D
                 // Arguments:
                 //   The type of texture we're generating. There are various different types of textures, but the only one we need right now is Texture2D.
                 //   Level of detail. We can use this to start from a smaller mipmap (if we want), but we don't need to do that, so leave it at 0.
-                //   Target format of the pixels.
+                //   Target format of the pixels. This is the format OpenGL will store our image with.
                 //   Width of the image
                 //   Height of the image.
                 //   Border of the image. This must always be 0; it's a legacy parameter that Khronos never got rid of.
-                //   The format of the pixels, explained above.
+                //   The format of the pixels, explained above. Since we loaded the pixels as ARGB earlier, we need to use BGRA.
                 //   Data type of the pixels.
                 //   And finally, the actual pixels.
-                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.GetPixelSpan().ToArray());
+                GL.TexImage2D(TextureTarget.Texture2D,
+                    0,
+                    PixelInternalFormat.Rgba,
+                    image.Width,
+                    image.Height,
+                    0,
+                    PixelFormat.Bgra,
+                    PixelType.UnsignedByte,
+                    data.Scan0);
             }
             
             // Now that our texture is loaded, we need to set a few settings.
