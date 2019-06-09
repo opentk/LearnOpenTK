ae6d50861458aaebbe6befe96c3e2c66cf8cb726
void <theprismaticvoid@gmail.com>
2019-06-08 19:09:30 -0500

    Better comments on filter settings

--- Common/Texture.cs
+++ Common/Texture.cs
@@ -58,12 +58,13 @@ namespace LearnOpenTK.Common
                     data.Scan0);
             }
             
-            // Now that our texture is loaded, we need to set a few settings.
-            // If you don't include these settings, OpenTK will refuse to draw the texture.
+            // Now that our texture is loaded, we can set a few settings to affect how the image appears on rendering.
 
             // First, we set the min and mag filter. These are used for when the texture is scaled down and up, respectively.
             // Here, we use Linear for both. This means that OpenGL will try to blend pixels, meaning that textures scaled too far will look blurred.
             // You could also use (amongst other options) Nearest, which just grabs the nearest pixel, which makes the texture look pixelated if scaled too far.
+            // NOTE: The default settings for both of these are LinearMipmap. If you leave these as default but don't generate mipmaps,
+            // your image will fail to render at all (usually resulting in pure black instead).
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
             GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
 
