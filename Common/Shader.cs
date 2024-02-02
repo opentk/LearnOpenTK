ba3c79caf300b01ca32cb918a48dffead6cccd03
Michael Ludlum <mludlum@usefulsolutions.com>
2024-02-02 10:49:47 -0500

    added Shader.SetMatrix3

--- Common/Shader.cs
+++ Common/Shader.cs
@@ -176,6 +176,22 @@ namespace LearnOpenTK.Common
         }
 
         /// <summary>
+        /// Set a uniform Matrix4 on this shader
+        /// </summary>
+        /// <param name="name">The name of the uniform</param>
+        /// <param name="data">The data to set</param>
+        /// <remarks>
+        ///   <para>
+        ///   The matrix is transposed before being sent to the shader.
+        ///   </para>
+        /// </remarks>
+        public void SetMatrix3(string name, Matrix3 data)
+        {
+            GL.UseProgram(Handle);
+            GL.UniformMatrix3(_uniformLocations[name], true, ref data);
+        }
+
+        /// <summary>
         /// Set a uniform Vector3 on this shader.
         /// </summary>
         /// <param name="name">The name of the uniform</param>
